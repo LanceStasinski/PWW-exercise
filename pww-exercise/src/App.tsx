03a8291569508a214f0175542ed81139eb0166db
LanceStasinski <lance.stasinski@gmail.com>
2022-03-02 14:16:47 -0500

    style: Add theme colors and fonts

--- pww-exercise/src/App.tsx
+++ pww-exercise/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="font-Roboto-Slab text-tertiary-5">Hello world!</h1>;
 }
 
 export default App;
--- pww-exercise/tailwind.config.js
+++ pww-exercise/tailwind.config.js
@@ -1,9 +1,35 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#142B47",
+        "primary-2": "#1B3A61",
+        "primary-3": "#225089",
+        "primary-4": "#3068AD",
+        "primary-5": "#0074C1",
+        "primary-6": "#93A4BD",
+        "primary-7": "#35B0CB",
+        "secondary-1": "#415A7B",
+        "tertiary-1": "#8C6910",
+        "tertiary-2": "#D2B859",
+        "tertiary-3": "#F0EBDD",
+        "tertiary-4": "#F7F4ED",
+        "tertiary-5": "#E9261D",
+        "gray-1": "#000000",
+        "gray-2": "#333333",
+        "gray-3": "#757575",
+        "gray-4": "#C3C3C3",
+        "gray-5": "#CCCCCC",
+        "gray-6": "#E1E5EA",
+        "gray-7": "#F3F6F9",
+      },
+      fontFamily: {
+        "Open-Sans": ["Open Sans", "sans-serif"],
+        "Roboto-Condensed": ["Roboto-Condensed", "sans-serif"],
+        "Roboto-Slab": ["Roboto Slab", "serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
